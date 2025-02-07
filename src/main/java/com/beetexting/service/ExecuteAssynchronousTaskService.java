44a46e45c09a676ed538b88c8c19724d51ccff4c
MaheshwarBohi <121285995+MaheshwarBohi@users.noreply.github.com>
2025-02-07 14:00:59 +0530

    Update ExecuteAssynchronousTaskService.java

--- src/main/java/com/beetexting/service/ExecuteAssynchronousTaskService.java
+++ src/main/java/com/beetexting/service/ExecuteAssynchronousTaskService.java
@@ -1,10 +1,28 @@
 package com.beetexting.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import com.beetexting.constant.Constants;
+import com.beetexting.constant.TaskExceptionConstants;
+import com.beetexting.customException.BeetextingException;
+import com.beetexting.entity.Task;
+import com.beetexting.repository.TaskRepository;
+
 @Service
 public class ExecuteAssynchronousTaskService {
+
+	@Autowired
+	private TaskRepository taskRepository;
+
+	private static final Logger logger = LoggerFactory.getLogger(ExecuteAssynchronousTaskService.class);
 
 	public String executeTask() {
 		executeAssynchronousTask();
@@ -18,6 +36,25 @@ public class ExecuteAssynchronousTaskService {
 		 * TODO lets make Task entity status as COMPLETED and calling controller API to
 		 * initiate notification
 		 */
+
+		try {
+			List<Task> taskList = taskRepository.findAll();
+
+			taskList.stream().map(task -> {
+				task.setStatus(Constants.COMPLETED);
+				task = taskRepository.save(task);
+
+				/**
+				 * need to send mail via Spring Mail
+				 */
+
+				return task;
+			}).collect(Collectors.toList());
+		} catch (DataAccessException e) {
+			logger.error("failed at databse execution with exception: {}", e.getMessage());
+			throw new BeetextingException("failed at database execution", TaskExceptionConstants.DATABASE_EXCEPTION);
+		}
+
 	}
 
 }
